d4067367303419503aa61cd4ef155009568f6f49
Dipen Patel <patel.dip@husky.neu.edu>
2020-11-25 00:19:32 -0500

    [ADD] Added a file which stores all possible timezones

--- src/help.js
+++ src/help.js
@@ -1,17 +1,18 @@
 import chalk from 'chalk';
+import data from '../data/timezones'
 
 const helpMenus = {
-    main: `
+  main: `
     ${chalk.yellowBright('Input Format is : <username>: <message> ')}, for example, ${chalk.yellowBright('josh: hello')} 
     There will always be exactly one space between the username - if there are multiple spaces, N-1 of 
     them were part of the user’s message. 
     The bot will receive a copy of all messages sent to the channel, but only needs to respond to those 
     which start with an exclamation point, which we call “commands”.
     For example josh: ${chalk.yellowBright('!timeat America/Los_Angeles')} 
-
   `
 }
 
 export async function help() {
-    console.log(helpMenus.main);
+  console.log(helpMenus.main);
+  console.log(data.timezones[0])
 }
